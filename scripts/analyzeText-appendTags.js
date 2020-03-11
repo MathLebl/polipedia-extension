f18a1168b2b6ae99654aea019b26c865963df3fe
Thomas <soler.thomas.06@gmail.com>
2020-03-11 14:37:23 +0100

    added tooltip; changed style for name highlighting

--- scripts/analyzeText-appendTags.js
+++ scripts/analyzeText-appendTags.js
@@ -26,26 +26,50 @@ async function readAnalyzeDocumentText() {
     return {
       normalName: hash.name,
       formattedName: hash.name.normalize('NFD').replace(/[\u0300-\u036f]/g, "")}
-  });
+    });
   var namesInDoc = [];
   namesArray.forEach(function(hash) {
     if(normalizedDocText.includes(hash['formattedName'])) {
       namesInDoc.push(hash['normalName']);
     }
   })
+  var styleTags = `<style>
+  .polipediaName {
+    background: linear-gradient(180deg, rgba(255,255,255,0) 50%, rgba(49,39,130, 0.2) 50%);
+  }
+
+  .polipediaName:hover {
+    background: linear-gradient(180deg, rgba(255,255,255,0) 50%, rgba(49,39,130, 0.3) 50%);
+    cursor: help;
+  }
+  </style>`;
+  document.querySelector('body').insertAdjacentHTML('afterbegin', styleTags);
   namesInDoc.forEach(function(name) {
     regex = new RegExp(name, "g");
-    var styleTags = `<style>
-    .polipediaName {
-      background-color: yellow;
-    }
-    </style>`
-    var nameWithTags = `<span class="polipediaName">${name}</span>`
-    document.querySelector('body').insertAdjacentHTML('afterbegin', styleTags);
+    var nameWithTags = `<span class="polipediaName" title="Découvrir sur Polipedia">${name}</span>`
     document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(regex, nameWithTags);
   })
 }
 
 readAnalyzeDocumentText();
-
 console.log('Done')
+
+// Functions below are supposed to implement 'tippy.js' to create a tooltip around the politician names - need to implement it with webpack
+
+// function addTippyCDN() {
+//   var tags = `<script src="https://unpkg.com/@popperjs/core@2"></script>
+//               <script src="https://unpkg.com/tippy.js@6"></script>`
+//   document.querySelector('body').insertAdjacentHTML('beforeend', tags)
+// }
+
+// addTippyCDN();
+
+// function addTippy() {
+//   tippy('.polipediaName', {
+//     content: 'Analyze with Polipedia'
+//   })
+// }
+
+// addTippy();
+
+
